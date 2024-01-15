59ef79c7ef2427f9faab2a01d57f84c917fe5dce
Kaedon Chung <kaedonchung@gmail.com>
2024-01-14 21:37:20 -0800

    comment added.

--- src/asphaltart/NeighborhoodRunner.java
+++ src/asphaltart/NeighborhoodRunner.java
@@ -6,6 +6,7 @@ public class NeighborhoodRunner {
     public static void main(String[] args) {
 
         TrophyPainter T = new TrophyPainter();
+        //navigates to starting position and paints background.
         T.startingPosition();
         T.paintBall("black", "crimson", "white", "silver");
 
